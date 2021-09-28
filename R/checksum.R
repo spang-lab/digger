1a4a1537c48f039b6af446bc9b8c1cdeee668766
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2021-09-28 16:49:40 +0200

    add sha256sum

---
+++ R/checksum.R
@@ -0,0 +1,36 @@
+#' reads a file and computes its sha256sum.
+#'
+#' @param filename file to read
+#'
+#' @return sha256sum
+#'
+#' @export
+sha256sum <- function(filename) {
+  if( ! file.exists(filename) ) {
+    stop(paste0("file ", filename, " does not exist."))
+  }
+  f <- file(filename, "rb")
+  crc <- digest(readBin(f, raw(), file.info(filename)$size), algo="sha256", serialize=FALSE)
+  close(f)
+  return(crc)
+}
+
+#' checks if a file matches the given checksum
+#'
+#' @param filename file to check
+#' @param checksum sha256sum to check against
+#' @param error if set to false, don't throw but return FALSE.
+#'
+#' @return TRUE, if the file is correct. FALSE, if the file is incorrect and error=FALSE
+#'
+#' @export
+check <- function(filename, checksum, error=TRUE) {
+  crc <- sha256sum(filename)
+  if( crc == checksum ) {
+    return(TRUE)
+  }
+  if( error ) {
+    stop(paste0("file \"", filename, " has checksum ", crc, " but is expected to have ", checksum))
+  }
+  return(FALSE)
+}
