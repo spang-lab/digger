f7e34a572e455a11d53d2f8628000f5a6eeb9f20
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2021-09-28 15:40:21 +0200

    by default allow only deletion of diggeR datasets.

--- R/delete.R
+++ R/delete.R
@@ -2,17 +2,25 @@
 #'
 #' @param hash hash to delete
 #' @param quiet if set to TRUE, don't ask before deletion
+#' @param force if set to true, allow deletion of datasets that were not uploaded using diggeR
 #'
 #' @return nothing
 #' @export
-delete <- function(hash, quiet = FALSE) {
-  if( ! quiet ) {
+delete <- function(hash, quiet = FALSE, force = FALSE) {
+  if( ! quiet || ! force ) {
     meta <- metadata(hash)
-    yn <- readline(prompt = paste0(
-                    "Do you really want to remotely (= ON THE SERVER) delete the data set with hash \"",
-                    hash, "\" (name: \"", meta$name, "\") [Ny]"))
-    if( yn != "y" && yn != "Y" && yn != "j" && yn != "J" ) {
+    if( ! quiet ) {
+      yn <- readline(prompt = paste0(
+                      "Do you really want to remotely (= ON THE SERVER) delete the data set with hash \"",
+                      hash, "\" (name: \"", meta$name, "\") [Ny]"))
+      if( yn != "y" && yn != "Y" && yn != "j" && yn != "J" ) {
       stop("deletion aborted upon request")
+      }
+    }
+    if( ! force ) {
+      if( meta$generator$kind != "diggeR" ){
+        stop("dataset was not uploaded by diggeR and thus can have dependent data sets. Won't delete this dataset (set force to TRUE if you know what you are doing.)")
+      }
     }
   }
   hash <- resolve_hash(hash)
